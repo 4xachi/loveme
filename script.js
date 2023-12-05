713a705900f826f505dc6ef08fbfab4290d4ec64
4xachi <71332645+4xachi@users.noreply.github.com>
2023-12-04 20:44:52 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
-yesBtn.addEventListener("click", () =>{
+yesBtn.addEventListener("click", () => {
     question.innerHTML = "hehe, i love you too😚";
     gif.src = "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
+    hideButtons();
 });
 
 noBtn.addEventListener("mouseover", () => {
@@ -14,10 +15,16 @@ noBtn.addEventListener("mouseover", () => {
     const maxX = window.innerWidth - noBtnRect.width;
     const maxY = window.innerHeight - noBtnRect.height;
 
-
     const randomX = Math.floor(Math.random() * maxX);
     const randomY = Math.floor(Math.random() * maxY);
 
     noBtn.style.left = randomX + "px";
     noBtn.style.top = randomY + "px";
-})
+});
+
+function hideButtons() {
+    yesBtn.classList.add("hidden");
+    noBtn.classList.add("hidden");
+}
+
+
